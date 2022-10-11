23dd9706160ff4241cf3c93c372c687df2a07995
dlepke <deannalepke@gmail.com>
2022-10-10 21:38:55 -0700

    made basic landing page UI with search bar and list

--- app/src/main/java/com/example/intellitravel/SearchPage.java
+++ app/src/main/java/com/example/intellitravel/SearchPage.java
@@ -1,8 +1,14 @@
 package com.example.intellitravel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.SurfaceControl;
+
+import com.example.intellitravel.CountryListFragment;
+import com.example.intellitravel.R;
 
 public class SearchPage extends AppCompatActivity {
 
@@ -10,5 +16,10 @@ public class SearchPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CountryListFragment countryListFragment = new CountryListFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.linear_layout, countryListFragment).commit();
     }
 }
