7a23d77fd1cfc90112e3770638301151715cefea
Yurtle (Lachlan Butler) <yurtle.butler@gmail.com>
2022-10-12 19:04:07 -0700

    further details activity

---
+++ app/src/main/java/com/example/intellitravel/FurtherDetails.java
@@ -0,0 +1,16 @@
+package com.example.intellitravel;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class FurtherDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_further_details);
+    }
+}
