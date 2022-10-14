6d8eaae53233d9b132cabd34bebfb8a5ab02b514
Lachlan Butler <yurtle.butler@gmail.com>
2022-10-14 11:28:27 -0700

    Implemented dynamic country name on Further Details fragment

--- app/src/main/java/com/example/intellitravel/FurtherDetails.java
+++ app/src/main/java/com/example/intellitravel/FurtherDetails.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,16 +15,6 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class FurtherDetails extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public FurtherDetails() {
         // Required empty public constructor
     }
@@ -32,16 +23,13 @@ public class FurtherDetails extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param country String.
      * @return A new instance of fragment FurtherDetails.
      */
-    // TODO: Rename and change types and number of parameters
-    public static FurtherDetails newInstance(String param1, String param2) {
+    public static FurtherDetails newInstance(String country) {
         FurtherDetails fragment = new FurtherDetails();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString("country", country);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +37,18 @@ public class FurtherDetails extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_further_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_further_details, container, false);
+        if (getArguments() != null) {
+            String country = getArguments().getString("country");
+            TextView nameView = view.findViewById(R.id.countryNameView);
+            nameView.setText(country);
+        }
+        return view;
     }
 }
