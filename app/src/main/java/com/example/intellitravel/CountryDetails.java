05db154318055897d50676fe78c8615ce45fccd8
Prab Bains <pbains61@my.bcit.ca>
2022-11-15 14:49:34 -0800

    Implement and populate country details page from API

--- app/src/main/java/com/example/intellitravel/CountryDetails.java
+++ app/src/main/java/com/example/intellitravel/CountryDetails.java
@@ -1,10 +1,17 @@
 package com.example.intellitravel;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -14,9 +21,29 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.navigation.NavigationBarView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+import java.util.Locale;
+
 public class CountryDetails extends AppCompatActivity implements OnMapReadyCallback {
+    private final String url = "https://sub.yurtle.net/";
+    TextView countryInfo;
+    TextView riskLevel;
+    TextView riskString;
+    String country_advisories;
+    String country_info;
+    TextView diseasesTextview;
+    TextView officialLanguagesTextview;
+    TextView regionTextview;
+    TextView capitalCityTextview;
+    TextView currencyTextview;
+    TextView callingCodeTextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +52,13 @@ public class CountryDetails extends AppCompatActivity implements OnMapReadyCallb
         setContentView(R.layout.activity_country_details);
 
         Intent intent = getIntent();
-        String countryName = intent.getStringExtra("country_name");
+        String countryName = intent.getStringExtra("country_name").toLowerCase(Locale.ROOT);
         System.out.println("from country details");
         System.out.println(countryName);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-
         TextView countryNameView = findViewById(R.id.country_name);
-        countryNameView.setText(countryName);
-
+        countryNameView.setText(countryName.toUpperCase(Locale.ROOT));
         navView.setSelectedItemId(R.id.nav_search_home);
 
         navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -68,6 +93,19 @@ public class CountryDetails extends AppCompatActivity implements OnMapReadyCallb
                 .add(R.id.map_sub_view, mapFragment)
                 .commit();
         mapFragment.getMapAsync(this);
+
+        riskLevel = findViewById(R.id.risk_level);
+        riskString = findViewById(R.id.risk_description);
+        diseasesTextview = findViewById(R.id.disease_item);
+        officialLanguagesTextview = findViewById(R.id.official_languages);
+        regionTextview = findViewById(R.id.region);
+        capitalCityTextview = findViewById(R.id.capital_city);
+        currencyTextview = findViewById(R.id.currency);
+        callingCodeTextview = findViewById(R.id.calling_code);
+        String tempUrl = url + "countries/" + countryName;
+        country_info = tempUrl + "/info";
+        AsyncTaskRunner runner = new AsyncTaskRunner();
+        runner.execute(tempUrl);
     }
 
     @Override
@@ -76,5 +114,116 @@ public class CountryDetails extends AppCompatActivity implements OnMapReadyCallb
                 .position(new LatLng(0, 0))
                 .draggable(true)
                 .title("Marker"));
+    }
+
+    private class AsyncTaskRunner extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            RequestQueue queue = Volley.newRequestQueue(CountryDetails.this);
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, strings[0],
+                    null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        String country_name = response.getString("prettyName");
+                        String country_risk_level = response.getString("riskLevel");
+                        String country_risk_string = response.getString("riskString");
+                        country_advisories = response.getString("advisroyURI");
+
+                        riskLevel.setText("Risk Level: " + country_risk_level);
+                        riskString.setText(country_risk_string);
+
+                        JsonObjectRequest request2 = new JsonObjectRequest(
+                                Request.Method.GET, country_advisories, null,
+                                new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    StringBuilder disease = new StringBuilder();
+                                    JSONArray diseases = response.getJSONArray("diseases");
+                                    String disease_index;
+                                    for (int i = 0; i < diseases.length(); i++) {
+                                        disease_index = diseases.getString(i);
+                                        disease.append("- ").append(disease_index).append("\n");
+                                    }
+
+                                    diseasesTextview.setText(disease);
+
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, error -> Toast.makeText(CountryDetails.this, error.toString(),
+                                Toast.LENGTH_SHORT).show());
+                        queue.add(request2);
+
+
+                        JsonObjectRequest request3 = new JsonObjectRequest(Request.Method.GET,
+                                country_info, null, new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    String official_languages = "Official Language(s): ";
+                                    JSONObject languages = response.getJSONObject("languages");
+                                    JSONObject JSON_geo = response.getJSONObject("geo");
+                                    JSONObject JSON_calling_code = response.getJSONObject("dialling");
+                                    JSONArray calling_code_array = JSON_calling_code.getJSONArray("calling_code");
+                                    String calling_code = calling_code_array.getString(0);
+
+                                    String region = JSON_geo.getString("region");
+                                    String capital_city = response.getString("capital");
+
+                                    JSONObject JSON_currency_object = response.getJSONObject("currency");
+                                    Iterator<String> currency_object = JSON_currency_object.keys();
+                                    String currency_key = "";
+
+                                    // iterates over official languages and adds them to the screen
+                                    Iterator<String> keys = languages.keys();
+                                    while (keys.hasNext()) {
+                                        String key = keys.next();
+                                        String value = (String) languages.get(key);
+                                        official_languages = official_languages + value;
+                                        if (keys.hasNext()) {
+                                            official_languages = official_languages + ", ";
+                                        }
+                                    }
+
+                                    // iterates over currency object
+                                    while (currency_object.hasNext()) {
+                                        currency_key = currency_object.next();
+                                    }
+
+                                    JSON_currency_object = JSON_currency_object.getJSONObject(currency_key);
+                                    String currency = JSON_currency_object.getString("iso_4217_name");
+
+
+                                    officialLanguagesTextview.setText(official_languages);
+                                    regionTextview.setText(region);
+                                    capitalCityTextview.setText("Capital City: " + capital_city);
+                                    callingCodeTextview.setText("Calling Code: " + calling_code);
+                                    currencyTextview.setText("Currency: " + currency);
+
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, error -> Toast.makeText(CountryDetails.this, error.toString(),
+                                Toast.LENGTH_SHORT).show());
+
+                        queue.add(request3);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, error -> Toast.makeText(CountryDetails.this, error.toString(),
+                    Toast.LENGTH_SHORT).show());
+            queue.add(request);
+
+            return null;
+        }
     }
 }
